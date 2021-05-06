01878c89138e6ab611da879474e61c02a3f61a7b
Ankita Jain <aankitajain.2016@gmail.com>
2021-05-06 17:00:26 +0530

    commit 1

---
+++ Onlineshopnew/src/main/java/com/cg/onlineshop/TestController.java
@@ -0,0 +1,20 @@
+package com.cg.onlineshop;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class TestController {
+	
+	@RequestMapping(value="/sayHello")
+	public ResponseEntity<String> sayHello(){
+	return new ResponseEntity<String>("Hello To All",HttpStatus.OK);
+	}
+	@RequestMapping(value="/sayHi")
+	public ResponseEntity<String> sayHi(){
+	return new ResponseEntity<String>("Hi To All",HttpStatus.OK);
+	}
+
+}
---
+++ Onlineshopnew/src/test/java/com/cg/onlineshop/OnlineShopTest.java
@@ -0,0 +1,13 @@
+package com.cg.onlineshop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OnlineShopTest {
+
+	@Test
+	public void test1()
+	{
+		Assertions.fail();
+	}
+}
